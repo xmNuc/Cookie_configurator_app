856d738d31235b692d4bdcaaa1b9d7e053f7e815
xmNuc <xmnuc@o2.pl>
2021-10-21 00:05:08 +0000

    3rd cimmit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const { append } = require('vary');
+const hbs = require('express-handlebars');
 const { homeRouter } = require('./routes/home');
 const { configuratorRouter } = require('./routes/configurator');
 const { orderRouter } = require('./routes/order');
@@ -10,16 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
-app.use(express.urlencoded({
-    extended: true,
-}))
 
-app.engine('.hbs', hbs({extname: '.hbs'})):
+app.engine('.hbs', hbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-
 
 app.use('/', homeRouter);
 app.use('/configurator', configuratorRouter);
 app.use('/order', orderRouter);
 
-app.listen(3000, localhost);
+app.listen(3000, 'localhost', () => {
+  console.log(`Server is started on port 3000`);
+});
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const homeRouter = express.Router();
 
+homeRouter.get('/', (req, res) => {
+  res.render('home/index', {
+    cookie: {
+      base: 'light',
+      addons: ['coconut', 'honey'],
+    },
+  });
+});
+
 module.exports = {
   homeRouter,
 };
