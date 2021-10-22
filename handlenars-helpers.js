9e748e0c981d23a8a5bd9510b2eb13eac8a273ed
xmNuc <xmnuc@o2.pl>
2021-10-22 01:10:52 +0000

    6th commit

--- handlenars-helpers.js
+++ handlenars-helpers.js
@@ -1,5 +1,15 @@
 const handlebarsHelpers = {
   upper: (str) => str.toUpperCase(),
+
+  'find-price': (entries, selectedItem) => {
+    const found = entries.find((el) => el[0] === selectedItem);
+
+    if (!found) {
+      throw new Error(`Cannot find price of "${selectedItem}".`);
+    }
+    const [, price] = found;
+    return price;
+  },
 };
 
 module.exports = {
