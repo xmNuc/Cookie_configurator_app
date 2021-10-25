b86f411165b41df662de723a792610fb1291825a
xmNuc <xmnuc@o2.pl>
2021-10-25 00:18:34 +0000

    update configurator, base-selected, addons

--- routes/configurator.js
+++ routes/configurator.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const configuratorRouter = express.Router();
 
+configuratorRouter.get('/select-base/:baseName', (req, res) => {
+  const { baseName } = req.params;
+  res.cookie('cookie-base', baseName).render('configurator/base-selected', {
+    baseName,
+  });
+});
+
 module.exports = {
   configuratorRouter,
 };
