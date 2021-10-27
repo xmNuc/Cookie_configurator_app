61c8e9df82b6dd537a729ed7df543befadb34b88
xmNuc <xmnuc@o2.pl>
2021-10-27 01:22:22 +0000

    9 commit

--- routes/configurator.js
+++ routes/configurator.js
@@ -21,8 +21,20 @@ configuratorRouter
       .render('configurator/added', {
         addonName,
       });
-  });
+  })
+  .get('/delete-addon/:addonName', (req, res) => {
+    const { addonName } = req.params;
 
+    const addons = getAddonsdFromReq(req).filter(
+      (addon) => addon !== addonName
+    );
+
+    res
+      .cookie('cookieAddons', JSON.stringify(addons))
+      .render('configurator/deleted', {
+        addonName,
+      });
+  });
 module.exports = {
   configuratorRouter,
 };
