0328fbfdc41f5b5a41d79c30de8fac3518ba7a8f
xmNuc <xmnuc@o2.pl>
2021-10-28 01:03:12 +0000

    9.6 commit

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies-data');
+const { getAddonsdFromReq } = require('../utils/get-addons-from-req');
+const { handlebarsHelpers } = require('../utils/handlenars-helpers');
 
 const orderRouter = express.Router();
+
+orderRouter.get('/summary', (req, res) => {
+  const { cookieBase } = req.cookies;
+  const addons = getAddonsdFromReq(req);
+
+  const sum =
+    (cookieBase
+      ? handlebarsHelpers.findPrice(Object.entries(COOKIE_BASES), cookieBase)
+      : 0) +
+    addons.reduce((prev, curr) => {
+      return (
+        prev + handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr)
+      );
+    }, 0);
+
+  res.render('order/summary', {
+    cookie: {
+      base: cookieBase,
+      addons: addons,
+    },
+    bases: Object.entries(COOKIE_BASES),
+    addons: Object.entries(COOKIE_ADDONS),
+    sum,
+  });
+});
 
 module.exports = {
   orderRouter,
