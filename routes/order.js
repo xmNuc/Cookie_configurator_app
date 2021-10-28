6c72905ce58e89832e392edd0b4248c95254ea8f
xmNuc <xmnuc@o2.pl>
2021-10-28 22:13:01 +0000

    9.8 commit

--- routes/order.js
+++ routes/order.js
@@ -5,30 +5,55 @@ const { handlebarsHelpers } = require('../utils/handlenars-helpers');
 
 const orderRouter = express.Router();
 
-orderRouter.get('/summary', (req, res) => {
-  const { cookieBase } = req.cookies;
-  const addons = getAddonsdFromReq(req);
+orderRouter
+  .get('/summary', (req, res) => {
+    const { cookieBase } = req.cookies;
+    const addons = getAddonsdFromReq(req);
 
-  const sum =
-    (cookieBase
-      ? handlebarsHelpers.findPrice(Object.entries(COOKIE_BASES), cookieBase)
-      : 0) +
-    addons.reduce((prev, curr) => {
-      return (
-        prev + handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr)
-      );
-    }, 0);
+    const sum =
+      (cookieBase
+        ? handlebarsHelpers.findPrice(Object.entries(COOKIE_BASES), cookieBase)
+        : 0) +
+      addons.reduce((prev, curr) => {
+        return (
+          prev +
+          handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr)
+        );
+      }, 0);
 
-  res.render('order/summary', {
-    cookie: {
-      base: cookieBase,
-      addons: addons,
-    },
-    bases: Object.entries(COOKIE_BASES),
-    addons: Object.entries(COOKIE_ADDONS),
-    sum,
+    res.render('order/summary', {
+      cookie: {
+        base: cookieBase,
+        addons: addons,
+      },
+      bases: Object.entries(COOKIE_BASES),
+      addons: Object.entries(COOKIE_ADDONS),
+      sum,
+    });
+  })
+
+  .get('/thanks', (req, res) => {
+    const { cookieBase } = req.cookies;
+    const addons = getAddonsdFromReq(req);
+
+    const sum =
+      (cookieBase
+        ? handlebarsHelpers.findPrice(Object.entries(COOKIE_BASES), cookieBase)
+        : 0) +
+      addons.reduce((prev, curr) => {
+        return (
+          prev +
+          handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr)
+        );
+      }, 0);
+
+    res
+      .clearCookie('cookieBase')
+      .clearCookie('cookieAddons')
+      .render('order/thanks', {
+        sum,
+      });
   });
-});
 
 module.exports = {
   orderRouter,
